8310f6b43b26b858cfc333511aee313b39647532
ArefinAzizSifat <aarefin.a.sifat22@gmail.com>
2026-02-27 15:21:29 +0100

    Add emoji to copyright

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -105,7 +105,7 @@ const Footer = () => {
         </div>
 
         <div className="border-t border-slate-200 dark:border-slate-800 pt-8 text-center text-sm text-slate-600 dark:text-slate-400">
-          <p>© {currentYear} Arefin Aziz Sifat.</p>
+          <p>© {currentYear} Arefin Aziz Sifat 💙</p>
         </div>
       </div>
     </footer>
